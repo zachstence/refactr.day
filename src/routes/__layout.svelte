cb9a3c83af3ed7419631e1357942a25ac633b6c2
Zachary Stence <zachstence@gmail.com>
2022-06-30 19:20:27 -0500

    feat: background image

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,4 +2,18 @@
     import "../app.css";
 </script>
 
-<slot />
+
+<main>
+    <slot />
+</main>
+
+
+<style lang="postcss">
+    main {
+        width: 100vw;
+        height: 100vh;
+
+        background: url('/background-tile-dark.png');
+        background-cover: repeat;
+    }
+</style>
